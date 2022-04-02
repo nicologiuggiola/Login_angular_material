c125109e2bb5ca99e4dcb4232e13c6f8781ff681
vivianabobbio <93972864+nicologiuggiola@users.noreply.github.com>
2022-04-02 13:05:07 +0200

    push2

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/login.service';
 
 @Component({
   selector: 'app-register-page',
@@ -8,17 +9,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterPageComponent implements OnInit {
 
+  value:string = ""
+
   profileForm = new FormGroup({
     username: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
-    chackPassword: new FormControl('', Validators.required),
+    checkPassword: new FormControl('', Validators.required),
     dob: new FormControl('', Validators.required)
   });
 
-  constructor() { }
+  constructor(private login:LoginService) { }
 
   ngOnInit(): void {
   }
 
+  onClickRegister(){
+    const user = {username: this.profileForm.value.username, password:this.profileForm.value.password, email:this.profileForm.value.email, dob:this.profileForm.value.dob}
+    this.login.searchBeforeRegister(user);
+    this.login.newRegisterCheck$.subscribe((users) => {
+      console.log(users.length);
+      
+      if (users.length < 1) {
+        this.value = "";
+        this.login.registerNewProfile(user).subscribe(user => console.log(user))
+      } else {
+        this.value = users.length + "";
+        console.log("false", this.value);
+      }})
+    
+  }
 }
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 
@@ -9,6 +9,7 @@ export class LoginService {
 
   private readonly apiUrl = 'https://6229de55be12fc4538aa6c8e.mockapi.io/users';
   public usernameCheck$ = new BehaviorSubject<any[]>([]);
+  public newRegisterCheck$ = new BehaviorSubject<any[]>([]);
 
 
   constructor(private http: HttpClient) { }
@@ -17,5 +18,21 @@ export class LoginService {
     this.http.get<any[]>(this.apiUrl + '?username=' + username).pipe(
       map((users) => users.filter((user:any) => user.password === password))
     ).subscribe((users) => this.usernameCheck$.next(users))
+  }
+
+  searchBeforeRegister(user:any){
+    this.http.get<any[]>(this.apiUrl + '?username=' + user.username).pipe(
+      map((users) => users.filter((user:any) => user.password === user.password && user.email === user.email))
+    ).subscribe((users) => this.usernameCheck$.next(users))
+  }
+
+  registerNewProfile(tempUser:any){
+    tempUser.dob = new Date(tempUser.dob).getTime();
+    const httpOptions = {headers: new HttpHeaders({ 'Content-Type': 'application/json'})}
+    return this.http.post<any>(this.apiUrl, tempUser, httpOptions).pipe(
+      map((user:any) => {
+        console.log(user);
+        return user;
+      }))
   }
 }
